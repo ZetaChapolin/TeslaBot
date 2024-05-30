ef0350b888ef32d8019edaf9fa209cc18b06291b
ZetaChapolin <165911506+ZetaChapolin@users.noreply.github.com>
2024-05-29 23:52:05 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -252,7 +252,7 @@ def run_script(email, password, account, par, entry_value, gales, stop_loss, sto
     speak_text("Tésla iniciado com sucesso. Aguarde e seja paciente, te avisarei quando acontecer alguma operação.")
     display_message("==========================================================================================")
     display_message("")
-    display_message("TRABALHANDO... AGUARDE E SEJA PACIENTE!")
+    display_message("TRABALHANDO... AGUARDE E SEJA PACIENTE! V_1")
     display_message("TE AVISAREI QUANDO ACONTECER ALGUMA OPERAÇÃO!")
     display_message("==========================================================================================")
     display_message("")
