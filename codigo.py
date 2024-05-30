f9ad22e1123820d6231ed788f628de098265da62
ZetaChapolin <165911506+ZetaChapolin@users.noreply.github.com>
2024-05-29 22:08:41 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -320,8 +320,8 @@ def run_script(email, password, account, par, entry_value, gales, stop_loss, sto
                     entrar = True if minutos % 15 == 0 else False 
 
                 if selected_option == 'M30':
-                    horarios_desejados = [21, 23, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
-                    entrar = True if agora in horarios_desejados and minutos == 0 and segundos == 0 else False
+                    horarios_desejados = ['21', '23', '1', '3', '5', '7', '9', '11', '13', '15', '17', '19', '21']
+                    entrar = True if hora_atual in horarios_desejados and minutos == 0 and segundos == 0 else False
 
     
 
