971815a44e569f694935c1863e284d221f0fc19f
ZetaChapolin <165911506+ZetaChapolin@users.noreply.github.com>
2024-08-24 13:55:17 -0300

    Update 3=1.py

--- TeslaPortable_refactored_v2.py
+++ TeslaPortable_refactored_v2.py
@@ -1,40 +1,13 @@
-import requests
-import importlib.util
-import os
-
-def download_file(url, file_path):
-    """Baixa um arquivo do URL fornecido e salva no caminho especificado."""
-    response = requests.get(url)
-    response.raise_for_status()  # Levanta um erro se o download falhar
-    with open(file_path, 'wb') as f:
-        f.write(response.content)
-
-def execute_script(file_path):
-    """Executa o script Python a partir do caminho fornecido."""
-    spec = importlib.util.spec_from_file_location("bot_module", file_path)
-    bot_module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(bot_module)
-
-def main():
-    url = 'https://github.com/ZetaChapolin/TeslaBot/raw/main/codigo.py'
-    file_path = 'codigo.py'
-    
-    try:
-        # Baixar o arquivo
-        print("Baixando o arquivo...")
-        download_file(url, file_path)
-        print("Arquivo baixado com sucesso.")
-        
-        # Executar o script
-        print("Executando o script...")
-        execute_script(file_path)
-        print("Script executado com sucesso.")
-        
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
-    finally:
-        if os.path.exists(file_path):
-            os.remove(file_path)  # Remove o arquivo temporário
-
-if __name__ == '__main__':
-    main()
+if selected_option == '3ª = 1ª':
+    display_message("================3ª = 1ª=============================================================================")
+    display_message("")
+    display_message('VERIFICANDO: ' + str(par) + ' às ' + datetime.now().strftime("%H:%M:%S"))
+    display_message("")
+    time.sleep(118)
+    velas = API.get_candles(par, 60, 1, time.time())
+    velas[0] = 'g' if velas[0]['open'] < velas[0]['close'] else 'r' if velas[0]['open'] > velas[0]['close'] else 'd'
+    cores = velas[0]
+    display_message(cores)
+    if signal == "COMPRA" and preco_atual > media_movel and velas[0] == 'g' and cores.count('d') == 0: dir = 'call'
+    if signal == "VENDA" and preco_atual < media_movel and velas[0] == 'r' and cores.count('d') == 0: dir = 'put'
+                         
