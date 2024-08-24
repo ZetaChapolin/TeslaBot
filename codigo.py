221040a9839553cf13863a00f2dadd44480879be
ZetaChapolin <165911506+ZetaChapolin@users.noreply.github.com>
2024-08-24 15:49:15 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -327,21 +327,21 @@ def run_script(email, password, account, par, entry_value, gales, stop_loss, sto
                     
                                 
                         #=======================================================3ª = 1ª==============================================================
-                        if selected_option == '3ª = 1ª':
-                            display_message("================3ª = 1ª=============================================================================")
-                            display_message("")
-                            display_message('VERIFICANDO: ' + str(par) + ' às ' + datetime.now().strftime("%H:%M:%S"))
-                            display_message("")
-                            #time.sleep(118)
-                            velas = API.get_candles(par, 60, 1, time.time())
-                            velas[0] = 'g' if velas[0]['open'] < velas[0]['close'] else 'r' if velas[0]['open'] > velas[0]['close'] else 'd'
+                    if selected_option == '3ª = 1ª':
+                        display_message("================3ª = 1ª=============================================================================")
+                        display_message("")
+                        display_message('VERIFICANDO: ' + str(par) + ' às ' + datetime.now().strftime("%H:%M:%S"))
+                        display_message("")
+                        #time.sleep(118)
+                        velas = API.get_candles(par, 60, 1, time.time())
+                        velas[0] = 'g' if velas[0]['open'] < velas[0]['close'] else 'r' if velas[0]['open'] > velas[0]['close'] else 'd'
                             
-                            cores = velas[0]
+                        cores = velas[0]
                             
-                            display_message(cores)
+                        display_message(cores)
                             
-                            if signal == "COMPRA" and preco_atual > media_movel and velas[0] == 'g' and cores.count('d') == 0: dir = 'call'
-                            if signal == "VENDA" and preco_atual < media_movel and velas[0] == 'r' and cores.count('d') == 0: dir = 'put'
+                        if signal == "COMPRA" and preco_atual > media_movel and velas[0] == 'g' and cores.count('d') == 0: dir = 'call'
+                        if signal == "VENDA" and preco_atual < media_movel and velas[0] == 'r' and cores.count('d') == 0: dir = 'put'
                        
 
                         
