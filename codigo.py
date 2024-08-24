1e081b56f5b7d0fd678e772f32ab6c7b060f3e7b
ZetaChapolin <165911506+ZetaChapolin@users.noreply.github.com>
2024-08-24 14:37:45 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -139,7 +139,7 @@ def run_script(email, password, account, par, entry_value, gales, stop_loss, sto
         return d
 
 
-    def IA(signal):
+    def IA():
         
 
         diferenca = preco_atual - media_movel
@@ -336,26 +336,7 @@ def run_script(email, password, account, par, entry_value, gales, stop_loss, sto
                     media_movel = sum(candle['close'] for candle in candles[:-1]) / 21
 
 
-                        #=======================================================3ª = 1ª==============================================================
-                    if selected_option == '3ª = 1ª':
-                        display_message("================3ª = 1ª=============================================================================")
-                        display_message("")
-                        display_message('VERIFICANDO: ' + str(par) + ' às ' + datetime.now().strftime("%H:%M:%S"))
-                        display_message("")
-                        time.sleep(118)
-                        velas = API.get_candles(par, 60, 2, time.time())
-                        velas[0] = 'g' if velas[0]['open'] < velas[0]['close'] else 'r' if velas[0]['open'] > velas[0]['close'] else 'd'
-                            
-                        cores = velas[0] + velas[1]
-                            
-                        display_message(cores)
-                            
-                        if signal == "COMPRA" and preco_atual > media_movel and velas[0] == 'g' and cores.count('d') == 0: dir = 'call'
-                        if signal == "VENDA" and preco_atual < media_movel and velas[0] == 'r' and cores.count('d') == 0: dir = 'put'
-                        
-                                    
-                      
-
+                    
                         
                     if dir:
                         
